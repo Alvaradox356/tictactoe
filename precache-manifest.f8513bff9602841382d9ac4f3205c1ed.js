83d75b9ae7880608c2796b14e508de2334f7a33e
Alvaradox356 <00144318@uca.edu.sv>
2019-10-30 11:35:25 -0600

    Updates

---
+++ precache-manifest.f8513bff9602841382d9ac4f3205c1ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f51914832bf5ffdfdd2e79feebdd2c2",
+    "url": "Alvaradox356.github.io/tictactoe/index.html"
+  },
+  {
+    "revision": "6cf417d2cd4e336d8488",
+    "url": "Alvaradox356.github.io/tictactoe/static/css/main.3fa7586b.chunk.css"
+  },
+  {
+    "revision": "7144a41f7e6c783a7e97",
+    "url": "Alvaradox356.github.io/tictactoe/static/js/2.83a1039e.chunk.js"
+  },
+  {
+    "revision": "6cf417d2cd4e336d8488",
+    "url": "Alvaradox356.github.io/tictactoe/static/js/main.bff8887a.chunk.js"
+  },
+  {
+    "revision": "763f18e29a0fb238cde1",
+    "url": "Alvaradox356.github.io/tictactoe/static/js/runtime-main.e5742fa9.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "Alvaradox356.github.io/tictactoe/precache-manifest.f8513bff9602841382d9ac4f3205c1ed.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("Alvaradox356.github.io/tictactoe/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
